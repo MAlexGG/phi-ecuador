78c8547e3b14c1af14447691043c7ecbc40fc41f
MAlexGG <alexandragalarzag@gmail.com>
2025-02-22 12:22:15 +0100

    logo universidad de guayaquil

--- app/data/universities.js
+++ app/data/universities.js
@@ -13,10 +13,16 @@ export const universities = [
         image: '/img/logos/ucacue.jpg',
         href: 'https://www.ucacue.edu.ec/',
         alt: 'logo de la universidad católica de Cuenca'
-    },{
+    },
+    {
         image: '/img/logos/ucsg.jpg',
         href: 'https://www.ucsg.edu.ec/',
         alt: 'logo de la universidad católica de Santiago de Guayaquil'
+    },
+    {
+        image: '/img/logos/ug.jpg',
+        href: 'https://www.ug.edu.ec/',
+        alt: 'logo de la universidad de Guayaquil'
     }
 
 ]
--- app/layout.js
+++ app/layout.js
@@ -14,7 +14,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={roboto.className}>{children}</body>
+      {<body className={roboto.className}>{children}</body>}
     </html>
   )
 }
