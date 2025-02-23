3b09f04df3d046a54d91ae0d200c60d8b4647c81
MAlexGG <alexandragalarzag@gmail.com>
2025-02-23 20:02:02 +0100

    github logo instead my name

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,12 +1,20 @@
 import Link from 'next/link'
 import React from 'react'
 import styles from './footer.module.css'
+import gh from '../../../public/icons/github.png'
+import Image from 'next/image'
 
 function Footer() {
   return (
     <div className={styles.ctFooter}>
         <p>RED PHI ECUADOR © Todos los derechos reservados</p>
-        <p>Desarrollado por <Link href='https://malexgg.github.io/portfolio/#/' target='_blank' rel="noopener noreferrer">MAlexGG</Link></p>
+        <Link className={styles.link} href='https://github.com/MAlexGG' target='_blank' rel="noopener noreferrer">
+          <Image
+            src={gh}
+            alt='github page María Alexandra Galarza Gallardo software developer'
+            width={30}
+          />
+        </Link>
     </div>
   )
 }
