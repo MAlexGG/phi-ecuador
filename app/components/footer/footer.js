1ae4903a2cdedf66be77a3677daf7e433622c9a6
MAlexGG <alexandragalarzag@gmail.com>
2025-10-04 11:35:25 +0200

    creator in footer deleted

--- app/components/footer/footer.js
+++ app/components/footer/footer.js
@@ -1,20 +1,10 @@
-import Link from 'next/link'
 import React from 'react'
 import styles from './footer.module.css'
-import gh from '../../../public/icons/github.png'
-import Image from 'next/image'
 
 function Footer() {
   return (
     <div className={styles.ctFooter}>
         <p>RED PHI ECUADOR © Todos los derechos reservados</p>
-        <Link className={styles.link} href='https://github.com/MAlexGG' target='_blank' rel="noopener noreferrer">
-          <Image
-            src={gh}
-            alt='github page María Alexandra Galarza Gallardo software developer'
-            width={30}
-          />
-        </Link>
     </div>
   )
 }
